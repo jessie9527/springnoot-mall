84ec1ed041f315f963b49ded7e2ef43c1b1cb9f9
jessie <milktea991107@gmail.com>
2023-10-25 20:07:02 +0800

    商品功能 - 實作修改商品

--- springboot-mall/src/main/java/com/jessie/springbootmall/controller/ProductController.java
+++ springboot-mall/src/main/java/com/jessie/springbootmall/controller/ProductController.java
@@ -41,4 +41,22 @@ public class ProductController {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(product);
     }
+
+//  刪除商品
+    @PutMapping("/products/{productId}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Integer productId,
+                                                 @RequestBody @Valid ProductRequest productRequest) {
+//      檢查商品是否存在
+        Product product = productService.getProductById(productId);
+
+        if (product == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+//      修改商品的數據
+        productService.updateProduct (productId, productRequest);
+
+        Product updateProduct = productService.getProductById(productId);
+
+        return  ResponseEntity.status(HttpStatus.OK).body(updateProduct);
+    }
 }
