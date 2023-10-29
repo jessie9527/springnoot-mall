8f3c379ca0111c09237819bfb0aec5ecb0e1fff6
jessie <milktea991107@gmail.com>
2023-10-29 16:37:34 +0800

    商品功能 - 實作查詢商品列表(查詢條件)

--- springboot-mall/src/main/java/com/jessie/springbootmall/controller/ProductController.java
+++ springboot-mall/src/main/java/com/jessie/springbootmall/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.jessie.springbootmall.controller;
 
+import com.jessie.springbootmall.constant.ProductCategory;
 import com.jessie.springbootmall.dto.ProductRequest;
 import com.jessie.springbootmall.model.Product;
 import com.jessie.springbootmall.service.ProductService;
@@ -19,8 +20,11 @@ public class ProductController {
 
 //  查詢商品列表(返回List列表)
     @GetMapping("/products")  // "/products":取得一堆商品、"/products/{productId}":取得一堆商品中特定的其中一個
-    public ResponseEntity<List<Product>> getProducts() {
-        List<Product> productList = productService.getProducts();
+    public ResponseEntity<List<Product>> getProducts(
+            @RequestParam(required = false) ProductCategory category,
+            @RequestParam(required = false) String search
+    ) {
+        List<Product> productList = productService.getProducts(category, search);
 
         return ResponseEntity.status(HttpStatus.OK).body(productList);
     }
--- springboot-mall/src/main/java/com/jessie/springbootmall/service/impl/ProductServiceImpl.java
+++ springboot-mall/src/main/java/com/jessie/springbootmall/service/impl/ProductServiceImpl.java
@@ -1,5 +1,6 @@
 package com.jessie.springbootmall.service.impl;
 
+import com.jessie.springbootmall.constant.ProductCategory;
 import com.jessie.springbootmall.dao.ProductDao;
 import com.jessie.springbootmall.dto.ProductRequest;
 import com.jessie.springbootmall.model.Product;
@@ -16,8 +17,8 @@ public class ProductServiceImpl implements ProductService {
     private ProductDao productDao;
 
     @Override
-    public List<Product> getProducts() {
-        return productDao.getProducts();
+    public List<Product> getProducts(ProductCategory category, String search) {
+        return productDao.getProducts(category, search);
     }
 
     @Override
